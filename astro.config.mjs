183064b92203d8e7739877f1547990c68ca72a06
Elias934JJB <163748626+Elias934JJB@users.noreply.github.com>
2025-05-05 01:57:09 +0200

    Fix theme colors and paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/portfolio-elias",
+  base: "/portfolio-elias/",
   site: "https://elias934jjb.github.io",
   integrations: [tailwind()],
+  build: {
+    assets: '_assets'
+  }
 });
